2c7e2ca761dc013a1e254168f3584f8d0f671b98
diablo <jaime@diablourbano.com>
2019-04-03 00:46:17 -0300

    add test for navigationUtils

---
+++ functional/__tests__/navigationUtils.spec.js
@@ -0,0 +1,66 @@
+import { styleGuides, drawerItems } from '../navigationUtils';
+
+describe('prepare drawerItems', () => {
+  it('should return empty drawer items', () => {
+    const drawer = drawerItems();
+    expect(drawer).toEqual([]);
+  });
+
+  it('should return drawer items with default values', () => {
+    const drawer = drawerItems([
+      {
+        id: 'foobar',
+      },
+    ]);
+    expect(drawer).toEqual([{
+      key: 'foobar',
+      routeName: 'foobar',
+      group: 'UNGROUPED',
+      title: 'foobar',
+      parent: 'NOPARENT',
+    }])
+  });
+
+  it('should return drawer items', () => {
+    const drawer = drawerItems([
+      {
+        id: 'foobar',
+        group: 'GROUP',
+        title: 'foo',
+        parent: 'SOMETHING',
+      },
+    ]);
+    expect(drawer).toEqual([{
+      key: 'foobar',
+      routeName: 'foobar',
+      group: 'GROUP',
+      title: 'foo',
+      parent: 'SOMETHING',
+    }])
+  });
+});
+
+describe.only('prepare styleGuides', () => {
+  it('should prepare components with default', () => {
+    const components = [{
+      id: 'foobar',
+    }];
+    const componentsToLoad = styleGuides(components);
+
+    expect(componentsToLoad).toEqual({
+      foobar: null,
+    });
+  });
+
+  it('should prepare components', () => {
+    const components = [{
+      id: 'foobar',
+      component: "hi, this is the component",
+    }];
+    const componentsToLoad = styleGuides(components);
+
+    expect(componentsToLoad).toEqual({
+      foobar: "hi, this is the component",
+    });
+  });
+});
--- functional/navigation.js
+++ functional/navigation.js
@@ -2,37 +2,11 @@ import { createDrawerNavigator } from 'react-navigation';
 import { STYLEGUIDE_SYSTEM } from '../utils/constants';
 import { COLORS } from '../utils/variables';
 import { Welcome, CustomDrawer, Styleguide } from '../index';
+import { styleGuides, drawerItems } from './navigationUtils';
 
 import '../../../styleguidesToLoad';
 
 const components = Styleguide.uiComponents();
-
-export const styleGuides = (componentsToParse) => {
-  const uiComponents = {};
-
-  componentsToParse.forEach(({ id, component }) => {
-    uiComponents[id] = component;
-  });
-
-  return uiComponents;
-}
-
-export const drawerItems = (componentsToParse) => {
-  return componentsToParse.map(({
-    id,
-    group = 'UNGROUPED',
-    title,
-    parent = 'NOPARENT',
-  }) => (
-    {
-      key: id,
-      routeName: id,
-      group,
-      title,
-      parent,
-    }
-  ));
-}
 
 export const StyleguideFlow = createDrawerNavigator({
   [STYLEGUIDE_SYSTEM]: Welcome,
