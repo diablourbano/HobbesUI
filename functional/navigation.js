62a5f05e9842875e0bdd249b1088c4bbf530a56a
diablo <jaime@diablourbano.com>
2021-06-11 18:14:14 -0300

    reverts HobbesNavigator to stateless component

--- functional/navigation.js
+++ functional/navigation.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-import { map, isEqual } from 'lodash';
+import {map} from 'lodash';
 import {HOBBESUI} from '../utils/constants';
 import {Welcome} from '../ui/Welcome';
 import {CustomDrawer} from '../ui/drawer';
@@ -10,77 +10,58 @@ import {drawerItems} from './navigationUtils';
 import {navigationRef, isNavigationReadyRef} from './refs';
 import '../../../styleguidesToLoad';
 
+const components = Hobbes.uiComponents();
+
+const customItems = [
+  {
+    key: HOBBESUI,
+    routeName: HOBBESUI,
+    title: 'Welcome to HobbesUI',
+  },
+  ...drawerItems(components),
+];
+
 const Drawer = createDrawerNavigator();
 
-export class HobbesNavigator extends Component {
-  constructor(props) {
-    super(props);
+export const HobbesNavigator = props => {
+  const {onLeaveHobbes} = props;
 
-    this.state = {
-      components:  Hobbes.uiComponents(),
-    };
-  }
-
-  componentDidUpdate() {
-    const { components } = this.state;
-    const newComponents = Hobbes.uiComponents();
-
-    if (!isEqual(components, newComponents)) {
-      this.setState({ components: Hobbes.uiComponents() });
-    }
-  }
-
-
-  render() {
-    const {onLeaveHobbes} = this.props;
-    const { components } = this.state;
-
-    const customItems = [
-      {
-        key: HOBBESUI,
-        routeName: HOBBESUI,
-        title: 'Welcome to HobbesUI',
-      },
-      ...drawerItems(components),
-    ];
-
-    return (
-      <NavigationContainer
-        ref={navigationRef}
-        onReady={() => {
-          isNavigationReadyRef.current = true;
-        }}
+  return (
+    <NavigationContainer
+      ref={navigationRef}
+      onReady={() => {
+        isNavigationReadyRef.current = true;
+      }}
+    >
+      <Drawer.Navigator
+        initialRouteName={HOBBESUI}
+        headerMode="screen"
+        headerShown={false}
+        drawerContent={drawerProps => (
+          <CustomDrawer
+            {...drawerProps}
+            customItems={customItems}
+            onLeaveHobbes={onLeaveHobbes}
+          />
+        )}
       >
-        <Drawer.Navigator
-          initialRouteName={HOBBESUI}
-          headerMode="screen"
-          headerShown={false}
-          drawerContent={drawerProps => (
-            <CustomDrawer
-              {...drawerProps}
-              customItems={customItems}
-              onLeaveHobbes={onLeaveHobbes}
-            />
-          )}
-        >
+        <Drawer.Screen
+          options={{headerTitle: null}}
+          key={HOBBESUI}
+          name={HOBBESUI}
+          component={Welcome}
+        />
+
+        {map(components, ({id: ScreenId, component: ScreenComponent}) => (
           <Drawer.Screen
             options={{headerTitle: null}}
-            key={HOBBESUI}
-            name={HOBBESUI}
-            component={Welcome}
-          />
-
-          {map(components, ({id: ScreenId, component: ScreenComponent}) => (
-            <Drawer.Screen
-              options={{headerTitle: null}}
-              key={ScreenId}
-              name={ScreenId}
-            >
-              {props => ScreenComponent}
-            </Drawer.Screen>
-          ))}
-        </Drawer.Navigator>
-      </NavigationContainer>
-    );
-  }
-}
+            key={ScreenId}
+            name={ScreenId}
+          >
+            {props => ScreenComponent}
+          </Drawer.Screen>
+        ))}
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+};
