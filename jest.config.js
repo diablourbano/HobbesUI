c247856b13e828beaf92520fab486b3614773a5c
diablo <jaime@diablourbano.com>
2023-12-29 03:43:12 -0300

    chore: removes example dir

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   preset: 'react-native',
   testMatch: ['<rootDir>/src/**/tests/*.test.(js|ts|jsx|tsx)'],
-  modulePathIgnorePatterns: [
-    '<rootDir>/example/node_modules',
-    '<rootDir>/lib/',
-  ],
+  modulePathIgnorePatterns: ['<rootDir>/lib/'],
   transform: {
     '^.+\\.(js|jsx)$': 'babel-jest',
     '^.+\\.(ts|tsx)$': 'ts-jest',
