3d31a2b1378a99dd41f9e22c8b76c8ef8dc7f5db
diablo <jaime@diablourbano.com>
2023-12-30 03:20:33 -0300

    refactor: removes unused colors

--- src/ui/resources.ts
+++ src/ui/resources.ts
@@ -1,21 +1,14 @@
 export const uiColors = {
   white: '#FFFFFF',
-  black: '#000000',
   primary: '#6D7953',
   secondary: '#905240',
   error: '#A68660',
-  warning: '#78797E',
-  note: '#78797E',
 
-  primaryBackground: 'rgba(109, 121, 83, 0.9)',
   primaryLightBackground: '#B9B883',
 
-  primaryShadow: '#444447',
   primaryLightShadow: '#6E6D4E',
   primaryLighterShadow: '#DBDBDB',
   primaryDarkShadow: '#292E18',
-
-  primaryContentShadow: '#424A26',
 
   contentBackground: '#4B4C4F',
 
